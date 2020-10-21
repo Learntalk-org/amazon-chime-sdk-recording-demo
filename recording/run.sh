8f37fff8351e7d576f76b29737ce390fbfd2c4be
andre <barinon@gmail.com>
2020-10-21 20:57:42 +0300

    entry point for chrome has neen added - 14

--- recording/run.sh
+++ recording/run.sh
@@ -61,7 +61,7 @@ google-chrome \
     --no-default-browser-check \
     --window-size='1280,720' \
   &
-sleep 0.5  # Ensure this has started before moving on
+sleep 5.5  # Ensure this has started before moving on
 xdotool mousemove 1 1 click 1  # Move mouse out of the way so it doesn't trigger the "pause" overlay on the video tile
 
 exec node /recording/record.js ${S3_BUCKET_NAME} ${SCREEN_WIDTH} ${SCREEN_HEIGHT}
